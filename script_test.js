961135d1de3b200fff708d713c00ddb3856a9981
guillaume-vives <guillaume.vives@sunr.com>
2025-08-11 00:29:37 +0200

    Test

--- script_test.js
+++ script_test.js
@@ -9,4 +9,23 @@ function initQuiz() {
     }
 }
 
-document.addEventListener("DOMContentLoaded", initQuiz);
+function loadData() {
+    const quizContainer = document.getElementById("quiz-container");
+    fetch("data/oeuvres.json") // Assurez-vous que le chemin est correct pour ce fichier
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Erreur lors du chargement du fichier JSON. Status: " + response.status);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(`${data.length} œuvres chargées avec succès`);
+            initQuiz(); // Appeler initQuiz après le chargement des données
+        })
+        .catch(error => {
+            console.error("Erreur:", error);
+            quizContainer.innerHTML = `<p>Erreur de chargement des données.</p>`; // Afficher un message d'erreur simple
+        });
+}
+
+document.addEventListener("DOMContentLoaded", loadData); // Appeler loadData au chargement du DOM
